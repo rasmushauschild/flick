bc880e6a0831832e964bd843fe21942392494bf5
Rasmus Hauschild <rasmushauschild@hotmail.com>
2026-04-27 19:42:34 +0200

    Initial Commit

---
+++ Flick/FlickApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlickApp.swift
+//  Flick
+//
+//  Created by Rasmus Hauschild on 27/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct FlickApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
